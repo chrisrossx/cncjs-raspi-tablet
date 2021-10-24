e954a24e7119e58bba10bae9940d2feef0aad545
Christopher Ross <chris.rossx@gmail.com>
2021-10-23 19:46:38 -0700

    Make Caps show first, for convience of GCode

--- src/keyboard.js
+++ src/keyboard.js
@@ -168,6 +168,22 @@ export class CNCKeyboard extends BaseKeyboard{
         this.keyboard = new Keyboard(".text-keyboard-instance",{
             onChange: input => { this.onChange(input) },
             onKeyPress: (button) => this.onKeyPress(button),
+            layout: {
+                'default': [
+                  '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+                  '{tab} Q W E R T Y U I O P [ ] \\',
+                  '{lock} A S D F G H J K L ; \' {enter}',
+                  '{shift} Z X C V B N M , . / {shift}',
+                  '.com @ {space}'
+                ],
+                'shift': [
+                  '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+                  '{tab} q w e r t y u i o p { } |',
+                  '{lock} a s d f g h j k l : " {enter}',
+                  '{shift} z x c v b n m < > ? {shift}',
+                  '.com @ {space}'
+                ]
+            }
         });
     }
 
